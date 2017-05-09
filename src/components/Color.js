ec63fcd134dd6f7bc0d389116f60d03ead994108
hrfmmymt <hrfmmymt@gmail.com>
2017-05-09 11:05:02 +0900

    modified

--- src/components/Color.js
+++ src/components/Color.js
@@ -2,48 +2,39 @@ import React from 'react'
 import { StyleSheet, css } from 'aphrodite'
 
 const styles = StyleSheet.create({
-  base: {
+  common: {
     color: '#000',
     fontSize: '16px',
     border: 'none',
-    backgroundColor: '#fff',
     padding: '1em'
   },
+  base: {
+    backgroundColor: '#fff',
+  },
   main: {
-    color: '#000',
-    fontSize: '16px',
-    border: 'none',
     backgroundColor: '#1ba1ff',
-    padding: '1em'
   },
   accent: {
     backgroundColor: '#1c5db5',
-    color: '#000',
-    fontSize: '16px',
-    border: 'none',
-    padding: '1em',
   },
   accentNotice: {
     backgroundColor: '#ffc11c',
-    color: '#000',
-    fontSize: '16px',
-    border: 'none',
-    padding: '1em',
-  }
+  },
 })
 
-const accent = {
-  backgroundColor: '#1c5db5',
-}
+Object.keys(styles).forEach(key => {
+  const val = styles[key]
+  console.log(key, val._definition)
+}, styles)
 
 const Color = ({ children, onClick }) => (
   <div>
     <h1>Theme Color</h1>
 
-    <div className={css(styles.base)} onClick={onClick}>Base Color</div>
-    <div className={css(styles.main)} onClick={onClick}>Main Color</div>
-    <div className={css(styles.accent)} onClick={onClick}>Accent Color {accent.backgroundColor}</div>
-    <div className={css(styles.accentNotice)} onClick={onClick}>Accent Color {accent.backgroundColor}</div>
+    <div className={css(styles.common, styles.base)} onClick={onClick}>Base Color {styles.base._definition.backgroundColor}</div>
+    <div className={css(styles.common, styles.main)} onClick={onClick}>Main Color {styles.main._definition.backgroundColor}</div>
+    <div className={css(styles.common, styles.accent)} onClick={onClick}>Accent Color {styles.accent._definition.backgroundColor}</div>
+    <div className={css(styles.common, styles.accentNotice)} onClick={onClick}>Accent Color {styles.accentNotice._definition.backgroundColor}</div>
 
     <textarea value={css(styles.base)}></textarea>
 
